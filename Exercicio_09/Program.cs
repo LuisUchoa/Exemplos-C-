526eb8cd3e25a447fba70c78ed4594d75d7c67dc
34822 <34822@L06M02>
2019-10-25 20:19:55 -0300

    Add project files.

---
+++ Exercicio_09/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, maior = 0, menor = 0;
+
+            do
+            {
+                Console.WriteLine("Informe um número: ");
+                num = int.Parse(Console.ReadLine());
+                if (num > maior)//se o numero for maior que o maior
+                {
+                    maior = num;//variavel armazena o maior valor dentre os lidos
+                }
+                else
+                {
+                    if (num < menor)//se o valor for menor que o menor
+                    {
+                        menor = num;//variavel armazena o menor dentre os lidos
+                    }
+                }
+            } while (num > 0);//enquanto o usuario nao digitar o negativo
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("O menor número é: " + menor);
+
+            Console.ReadKey();
+        }
+        
+    }
+}
